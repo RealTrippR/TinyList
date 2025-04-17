f52d1b556e0da24da731b58c44c120c13a821f1b
Tripp R <60992655+RealTrippR@users.noreply.github.com>
2025-04-17 19:53:52 -0400

    added comment to CREATE_LINKED_LIST

--- tinyList.h
+++ tinyList.h
@@ -256,7 +256,7 @@ inline void deleteNode_##NODE_TYPE\
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/*Creates a linked list of the given size*/
+/*Creates a linked list of a given size, and for very large lists it will use multithreading to speed up creation time.*/
 #define CREATE_LINKED_LIST(NODE_TYPE, size) createLinkedListOfSize##NODE_TYPE\
 (size)
 /*Allocates and returns a 0-initialized node*/
